879dc17c8423d10682443bc04c9c7f55bce98e15
Melinda Richards <richardsmelinda@yahoo.com>
2018-04-23 18:20:30 -0600

    bamazon

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -56,65 +56,66 @@ function start() {
 			.then(function (inquirerResponse) {
 				checkStock(inquirerResponse.id, inquirerResponse.amount);
 			});
-		})
+	})
+}
+
+// create a function to see if we have enough of the stock to meet the request
+function checkStock(responseId, responseAmount) {
+	console.log("working");
+	console.log(responseId);
+	console.log(responseAmount);
+
+
+	 connection.query("SELECT stock_quantity FROM products WHERE ?",
+		{ id: responseId },
+		
+		function (err, results) {
+			console.log(results);
+			if (err) throw err;
+
+	// If userAmount is less that stockAmount, subtract the former from the latter and create a new variable to store the result
+
+	var stockAmount = results[0].stock_quantity;
+	
+	var newAmount = stockAmount - responseAmount;
+			
+	// do the math to see if the user amount is greater than the amount in stock
+	if (responseAmount > stockAmount) {
+		console.log("Insufficient quantity!")
 	}
 
-		// create a function to see if we have enough of the stock to meet the request
-		function checkStock() {
-			console.log("working");
-
-
-// ******************* stops working here
-
-
-			// connection.query("SELECT stock_quantity FROM products WHERE id = " + itemID, function (err, results) {
-			connection.query("SELECT stock_quantity FROM products WHERE id = " + inquirerResponse.id, function (err, results) {
-				console.log(results);
+	else {
+		// update the database with the new amount of stock on hand
+		connection.query("UPDATE products SET stock_quantity = " + newAmount + "WHERE id = " + responseId, function(){
+			console.log("line 90");
+		});
+		
+		// query the database to find the price of the item selected by the user
+		connection.query("SELECT price FROM products WHERE id = " + responseId,
+			function (err, results) {
+				console.log("test");
 				if (err) throw err;
+				console.log(results);
 
-				// create a variable to store the ID of the item selected by the user
-				// create a variable to store the amount requested by the user 
-				// create a variable to store the amount of the product currently in stock
+				// create a variable to store the unit price
+				var itemPrice = results[0].price;
+				// ******************* how do I define this variable?
 
-				
-				var stockAmount;
-				// var stockAmount = results.something
+				// create a variable to store the total cost (the unit price multiplied by the number of units requested)
+				var totalCost;
+				var totalCost = itemPrice * responseAmount;
+				console.log("The total cost of your order is " + totalCost + ".");
 
+				// inquirer.prompt
+				// do you want to order again?
+				// if yes, start function
+				// if no, console log goodbye
 
-				// If userAmount is less that stockAmount, subtract the former from the latter and create a new variable to store the result
-
-				var newAmount;
-				var newAmount = stockAmount - userAmount;
-
-
-				// do the math to see if the user amount is greater than the amount in stock
-				if (userAmount > stockAmount) {
-					console.log("Insufficient quantity!")
-				}
-
-				else {
-					// update the database with the new amount of stock on hand
-					connection.query("UPDATE products SET stock_quantity = " + newAmount + "WHERE id = " + itemID);
-
-					// query the database to find the price of the item selected by the user
-					connection.query("SELECT price FROM products WHERE id = " + itemID,
-						function (err, results) {
-							if (err) throw err;
-							console.log(results);
-
-							// create a variable to store the unit price
-							var itemPrice;
-							// ******************* how do I define this variable?
-
-							// create a variable to store the total cost (the unit price multiplied by the number of units requested)
-							var totalCost;
-							var totalCost = itemPrice * userAmount;
-							console.log("The total cost of your order is " + totalCost + ".");
-							// run the start function to re-prompt the customer to place another order
-							start();
-						}
-					);
-				}
-				// Show total cost of order.
-			});
-		}
+				// run the start function to re-prompt the customer to place another order
+			}
+		);
+	}
+	
+});
+};
+// run the start function to re-prompt the customer to place another order
